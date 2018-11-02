eea603f965901ad721342abf8018b11567573f7a
Mishal Shah <shahmishal1998@gmail.com>
2018-11-02 02:33:38 -0700

    Ns3 code added (#2)
    * added ns3 code, started with the header file
    
    * removed the functions in class for starter code only

---
+++ ns3/src/internet/model/tcp-nv.h
@@ -0,0 +1,15 @@
+#ifndef TCPNEWVEGAS_H
+#define TCPNEWVEGAS_H
+
+#include "ns3/tcp-congestion-ops.h"
+#include "ns3/tcp-recovery-ops.h"
+
+namespace ns3 {
+
+class TcpNewVegas : public TcpNewReno
+{
+public:
+	
+};
+}
+#endif // TCPNEWVEGAS_H
